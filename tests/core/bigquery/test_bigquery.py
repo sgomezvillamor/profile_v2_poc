6f646379007580f7cb6bfe93fec37c40d90b599a
Sergio Gómez Villamor <sgomezvillamor@gmail.com>
2025-02-27 18:19:36 +0100

    test_group_requests_by_bigquerydataset

--- tests/core/bigquery/test_bigquery.py
+++ tests/core/bigquery/test_bigquery.py
@@ -256,6 +256,125 @@ class TestBigQueryInformationSchemaProfileEngine(unittest.TestCase):
 
 class TestBigQueryProfileEngine(unittest.TestCase):
 
+    requests = [
+        ProfileRequest(
+            statistics=[
+                TypedStatistic(
+                    name="row_count",
+                    fq_name="acryl-staging.customer_demo.PurchaseEvent.row_count",
+                    columns=[],  # Not used
+                    type=ProfileStatisticType.TABLE_ROW_COUNT,
+                ),
+            ],
+            batch=BatchSpec(
+                fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_CUSTOMER_DEMO}.PurchaseEvent",
+            ),
+        ),
+        ProfileRequest(
+            statistics=[
+                TypedStatistic(
+                    name="row_count",
+                    fq_name="acryl-staging.customer_demo.revenue.row_count",
+                    columns=[],  # Not used
+                    type=ProfileStatisticType.TABLE_ROW_COUNT,
+                ),
+            ],
+            batch=BatchSpec(
+                fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_CUSTOMER_DEMO}.revenue",
+            ),
+        ),
+        ProfileRequest(
+            statistics=[
+                TypedStatistic(
+                    name="row_count",
+                    fq_name="acryl-staging.deploy_test_1k.table_1.row_count",
+                    columns=[],  # Not used
+                    type=ProfileStatisticType.TABLE_ROW_COUNT,
+                ),
+            ],
+            batch=BatchSpec(
+                fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_DEPLOY_TEST_1K}.table_1",
+            ),
+        ),
+        ProfileRequest(
+            statistics=[
+                TypedStatistic(
+                    name="row_count",
+                    fq_name="acryl-staging.deploy_test_1k.table_10.row_count",
+                    columns=[],  # Not used
+                    type=ProfileStatisticType.TABLE_ROW_COUNT,
+                ),
+            ],
+            batch=BatchSpec(
+                fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_DEPLOY_TEST_1K}.table_10",
+            ),
+        ),
+        ProfileRequest(
+            statistics=[
+                TypedStatistic(
+                    name="distinct_count",
+                    fq_name="acryl-staging.deploy_test_1k.PurchaseEvent.product_id.distinct_count",
+                    columns=["product_id"],
+                    type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
+                ),
+            ],
+            batch=BatchSpec(
+                fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_CUSTOMER_DEMO}.PurchaseEvent",
+            ),
+        ),
+        ProfileRequest(
+            statistics=[
+                TypedStatistic(
+                    name="distinct_count",
+                    fq_name="acryl-staging.deploy_test_1k.PurchaseEvent.user_id.distinct_count",
+                    columns=["user_id"],
+                    type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
+                ),
+            ],
+            batch=BatchSpec(
+                fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_CUSTOMER_DEMO}.PurchaseEvent",
+            ),
+        ),
+        ProfileRequest(
+            statistics=[
+                CustomStatistic(
+                    name="distinct_count",
+                    fq_name="acryl-staging.deploy_test_1k.PurchaseEvent.amount.custom_avg",
+                    sql="CEIL(AVG(amount))",
+                ),
+            ],
+            batch=BatchSpec(
+                fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_CUSTOMER_DEMO}.PurchaseEvent",
+            ),
+        ),
+        ProfileRequest(
+            statistics=[
+                TypedStatistic(
+                    name="distinct_count",
+                    fq_name="acryl-staging.deploy_test_1k.deploy_test_1k.table_1.distinct_count",
+                    columns=["column_0"],
+                    type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
+                ),
+            ],
+            batch=BatchSpec(
+                fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_DEPLOY_TEST_1K}.table_1",
+            ),
+        ),
+    ]
+
+    def test_group_requests_by_bigquerydataset(self):
+        batch_requests = BigQueryProfileEngine._group_requests_by_bigquerydataset(self.requests)
+        print(batch_requests)
+        assert len(batch_requests) == 2
+        distinct_bigquery_datasets_batch_0 = set(BigQueryUtils.bigquerydataset_from_batch_spec(request.batch) for request in batch_requests[0])
+        distinct_bigquery_datasets_batch_1 = set(BigQueryUtils.bigquerydataset_from_batch_spec(request.batch) for request in batch_requests[1])
+        assert len(distinct_bigquery_datasets_batch_0)  == len(distinct_bigquery_datasets_batch_1) == 1
+        assert distinct_bigquery_datasets_batch_0 != distinct_bigquery_datasets_batch_1
+        assert distinct_bigquery_datasets_batch_0.union(distinct_bigquery_datasets_batch_1) == {
+            f"{BIGQUERY_DATASET_CUSTOMER_DEMO}",
+            f"{BIGQUERY_DATASET_DEPLOY_TEST_1K}",
+        }
+
     def test_integration_test(self):
         datasource = DataSource(
             name="bigquery",
@@ -264,113 +383,9 @@ class TestBigQueryProfileEngine(unittest.TestCase):
                 "credentials_path": BIGQUERY_CREDENTIALS_PATH,
             },
         )
-        requests = [
-            ProfileRequest(
-                statistics=[
-                    TypedStatistic(
-                        name="row_count",
-                        fq_name="acryl-staging.customer_demo.PurchaseEvent.row_count",
-                        columns=[],  # Not used
-                        type=ProfileStatisticType.TABLE_ROW_COUNT,
-                    ),
-                ],
-                batch=BatchSpec(
-                    fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_CUSTOMER_DEMO}.PurchaseEvent",
-                ),
-            ),
-            ProfileRequest(
-                statistics=[
-                    TypedStatistic(
-                        name="row_count",
-                        fq_name="acryl-staging.customer_demo.revenue.row_count",
-                        columns=[],  # Not used
-                        type=ProfileStatisticType.TABLE_ROW_COUNT,
-                    ),
-                ],
-                batch=BatchSpec(
-                    fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_CUSTOMER_DEMO}.revenue",
-                ),
-            ),
-            ProfileRequest(
-                statistics=[
-                    TypedStatistic(
-                        name="row_count",
-                        fq_name="acryl-staging.deploy_test_1k.table_1.row_count",
-                        columns=[],  # Not used
-                        type=ProfileStatisticType.TABLE_ROW_COUNT,
-                    ),
-                ],
-                batch=BatchSpec(
-                    fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_DEPLOY_TEST_1K}.table_1",
-                ),
-            ),
-            ProfileRequest(
-                statistics=[
-                    TypedStatistic(
-                        name="row_count",
-                        fq_name="acryl-staging.deploy_test_1k.table_10.row_count",
-                        columns=[],  # Not used
-                        type=ProfileStatisticType.TABLE_ROW_COUNT,
-                    ),
-                ],
-                batch=BatchSpec(
-                    fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_DEPLOY_TEST_1K}.table_10",
-                ),
-            ),
-            ProfileRequest(
-                statistics=[
-                    TypedStatistic(
-                        name="distinct_count",
-                        fq_name="acryl-staging.deploy_test_1k.PurchaseEvent.product_id.distinct_count",
-                        columns=["product_id"],
-                        type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
-                    ),
-                ],
-                batch=BatchSpec(
-                    fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_CUSTOMER_DEMO}.PurchaseEvent",
-                ),
-            ),
-            ProfileRequest(
-                statistics=[
-                    TypedStatistic(
-                        name="distinct_count",
-                        fq_name="acryl-staging.deploy_test_1k.PurchaseEvent.user_id.distinct_count",
-                        columns=["user_id"],
-                        type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
-                    ),
-                ],
-                batch=BatchSpec(
-                    fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_CUSTOMER_DEMO}.PurchaseEvent",
-                ),
-            ),
-            ProfileRequest(
-                statistics=[
-                    CustomStatistic(
-                        name="distinct_count",
-                        fq_name="acryl-staging.deploy_test_1k.PurchaseEvent.amount.custom_avg",
-                        sql="CEIL(AVG(amount))",
-                    ),
-                ],
-                batch=BatchSpec(
-                    fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_CUSTOMER_DEMO}.PurchaseEvent",
-                ),
-            ),
-            ProfileRequest(
-                statistics=[
-                    TypedStatistic(
-                        name="distinct_count",
-                        fq_name="acryl-staging.deploy_test_1k.deploy_test_1k.table_1.distinct_count",
-                        columns=["column_0"],
-                        type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
-                    ),
-                ],
-                batch=BatchSpec(
-                    fq_dataset_name=f"{BIGQUERY_PROJECT}.{BIGQUERY_DATASET_DEPLOY_TEST_1K}.table_1",
-                ),
-            ),
-        ]
+
         engine = BigQueryProfileEngine()
-        response = engine.do_profile(datasource, requests)
+        response = engine.do_profile(datasource, self.requests)
         print(response)
         assert response == ProfileResponse(
             data={
