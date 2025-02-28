7e09277c6362116b9f69b670e6d202c50f5ee654
Sergio Gómez Villamor <sgomezvillamor@gmail.com>
2025-02-28 11:33:07 +0100

    refactor tests

---
+++ tests/core/sqlalchemy/test_sqlalchemy.py
@@ -0,0 +1,207 @@
+import unittest
+
+from profile_v2.core.model import (BatchSpec, CustomStatistic, DataSource,
+                                   ProfileRequest, ProfileResponse,
+                                   ProfileStatisticType, SampleSpec,
+                                   SuccessStatisticResult, TypedStatistic)
+from profile_v2.core.sqlalchemy.sqlalchemy import SqlAlchemyProfileEngine
+from tests.core.common import (SNOWFLAKE_CONNECTION_STRING, SNOWFLAKE_DATABASE,
+                               SNOWFLAKE_SCHEMA)
+
+
+class TestSqlAlchemyProfileEngine(unittest.TestCase):
+
+    def test_api_distinct_count(self):
+        profile_engine = SqlAlchemyProfileEngine()
+
+        result = profile_engine.profile(
+            datasource=DataSource(
+                name="snowflake",
+                connection_string=SNOWFLAKE_CONNECTION_STRING,
+            ),
+            requests=[
+                ProfileRequest(
+                    statistics=[
+                        TypedStatistic(
+                            name=ProfileStatisticType.COLUMN_DISTINCT_COUNT.value,
+                            fq_name="SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.ID.distinct_count",
+                            columns=["ID"],
+                            type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
+                        ),
+                    ],
+                    batch=BatchSpec(
+                        fq_dataset_name=f"{SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.COVID19_EXTERNAL_TABLE"
+                    ),
+                )
+            ],
+        )
+        print(result)
+        assert result == ProfileResponse(
+            data={
+                "SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.ID.distinct_count": SuccessStatisticResult(
+                    value=398880
+                ),
+            },
+        )
+
+    def test_api_distinct_count_multiple(self):
+        profile_engine = SqlAlchemyProfileEngine()
+
+        result = profile_engine.profile(
+            datasource=DataSource(
+                name="snowflake",
+                connection_string=SNOWFLAKE_CONNECTION_STRING,
+            ),
+            requests=[
+                ProfileRequest(
+                    statistics=[
+                        TypedStatistic(
+                            name=ProfileStatisticType.COLUMN_DISTINCT_COUNT.value,
+                            fq_name="SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.ID.distinct_count",
+                            columns=["ID"],
+                            type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
+                        ),
+                        TypedStatistic(
+                            name=ProfileStatisticType.COLUMN_DISTINCT_COUNT.value,
+                            fq_name="SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.LABEL.distinct_count",
+                            columns=["LABEL"],
+                            type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
+                        ),
+                    ],
+                    batch=BatchSpec(
+                        fq_dataset_name=f"{SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.COVID19_EXTERNAL_TABLE"
+                    ),
+                )
+            ],
+        )
+        print(result)
+        assert result == ProfileResponse(
+            data={
+                "SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.ID.distinct_count": SuccessStatisticResult(
+                    value=398880
+                ),
+                "SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.LABEL.distinct_count": SuccessStatisticResult(
+                    value=5
+                ),
+            },
+        )
+
+    def test_api_custom_statistic(self):
+        profile_engine = SqlAlchemyProfileEngine()
+
+        result = profile_engine.profile(
+            datasource=DataSource(
+                name="snowflake",
+                connection_string=SNOWFLAKE_CONNECTION_STRING,
+            ),
+            requests=[
+                ProfileRequest(
+                    statistics=[
+                        CustomStatistic(
+                            name="custom_average_str_length",
+                            fq_name="SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.LABEL.custom_average_str_length",
+                            sql="CEIL(AVG(LEN(LABEL)))",
+                        ),
+                    ],
+                    batch=BatchSpec(
+                        fq_dataset_name=f"{SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.COVID19_EXTERNAL_TABLE"
+                    ),
+                )
+            ],
+        )
+        print(result)
+
+        assert result == ProfileResponse(
+            data={
+                "SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.LABEL.custom_average_str_length": SuccessStatisticResult(
+                    value=7
+                ),
+            },
+        )
+
+    def test_api_sample(self):
+        profile_engine = SqlAlchemyProfileEngine()
+
+        result = profile_engine.profile(
+            datasource=DataSource(
+                name="snowflake",
+                connection_string=SNOWFLAKE_CONNECTION_STRING,
+            ),
+            requests=[
+                ProfileRequest(
+                    statistics=[
+                        TypedStatistic(
+                            name=ProfileStatisticType.COLUMN_DISTINCT_COUNT.value,
+                            fq_name="SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.ID.distinct_count",
+                            columns=["ID"],
+                            type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
+                        ),
+                    ],
+                    batch=BatchSpec(
+                        fq_dataset_name=f"{SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.COVID19_EXTERNAL_TABLE",
+                        sample=SampleSpec(
+                            size=100,
+                        ),
+                    ),
+                )
+            ],
+        )
+        print(result)
+
+        assert result == ProfileResponse(
+            data={
+                "SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.ID.distinct_count": SuccessStatisticResult(
+                    value=100
+                ),
+            },
+        )
+
+    def test_api_different_datasets(self):
+        profile_engine = SqlAlchemyProfileEngine()
+
+        result = profile_engine.profile(
+            datasource=DataSource(
+                name="snowflake",
+                connection_string=SNOWFLAKE_CONNECTION_STRING,
+            ),
+            requests=[
+                ProfileRequest(
+                    statistics=[
+                        TypedStatistic(
+                            name=ProfileStatisticType.COLUMN_DISTINCT_COUNT.value,
+                            fq_name="SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.ID.distinct_count",
+                            columns=["ID"],
+                            type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
+                        ),
+                    ],
+                    batch=BatchSpec(
+                        fq_dataset_name=f"{SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.COVID19_EXTERNAL_TABLE",
+                    ),
+                ),
+                ProfileRequest(
+                    statistics=[
+                        TypedStatistic(
+                            name=ProfileStatisticType.COLUMN_DISTINCT_COUNT.value,
+                            fq_name="SMOKE_TEST_DB.PUBLIC.TABLE_FROM_S3_STAGE.FILENAME.distinct_count",
+                            columns=["FILENAME"],
+                            type=ProfileStatisticType.COLUMN_DISTINCT_COUNT,
+                        ),
+                    ],
+                    batch=BatchSpec(
+                        fq_dataset_name=f"{SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.TABLE_FROM_S3_STAGE",
+                    ),
+                ),
+            ],
+        )
+        print(result)
+
+        assert result == ProfileResponse(
+            data={
+                "SMOKE_TEST_DB.PUBLIC.COVID19_EXTERNAL_TABLE.ID.distinct_count": SuccessStatisticResult(
+                    value=398880
+                ),
+                "SMOKE_TEST_DB.PUBLIC.TABLE_FROM_S3_STAGE.FILENAME.distinct_count": SuccessStatisticResult(
+                    value=1
+                ),
+            },
+        )
